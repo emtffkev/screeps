08a77d6b1b0fe6373e7bf7b05668af27820af76f
Unknown <emtffkev@gmail.com>
2017-01-21 02:48:29 -0500

    Fixed Refactor

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
-// import modules
 require('prototype.creep');
 require('prototype.tower');
 require('prototype.spawn');
 
 module.exports.loop = function() {
-    // check for memory entries of died creeps by iterating over Memory.creeps
-    for (let name in Memory.creeps) {
-        // and checking if the creep is still alive
-        if (Game.creeps[name] == undefined) {
-            // if not, delete the memory entry
-            delete Memory.creeps[name];
-        }
+  for (let name in Memory.creeps) {
+    if (Game.creeps[name] == undefined) {
+      delete Memory.creeps[name];
     }
+  }
 
-    // for each creeps
-    for (let name in Game.creeps) {
-        // run creep logic
-        Game.creeps[name].runRole();
-    }
+  for (let name in Game.creeps) {
+    Game.creeps[name].runRole();
+  }
 
-    // find all towers
-    var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
-    // for each tower
-    for (let tower of towers) {
-        // run tower logic
-        tower.defend();
-    }
+  var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
+  for (let tower of towers) {
+    tower.defend();
+  }
 
-    // for each spawn
-    for (let spawnName in Game.spawns) {
-        // run spawn logic
-        Game.spawns[spawnName].spawnCreepsIfNecessary();
-    }
+  for (let spawnName in Game.spawns) {
+    Game.spawns[spawnName].spawnCreepsIfNecessary();
+  }
 };
--- role.claimer.js
+++ role.claimer.js
@@ -1,19 +1,12 @@
 module.exports = {
-    // a function to run the logic for this role
-    run: function(creep) {
-        // if in target room
-        if (creep.room.name != creep.memory.target) {
-            // find exit to target room
-            var exit = creep.room.findExitTo(creep.memory.target);
-            // move to exit
-            creep.moveTo(creep.pos.findClosestByRange(exit));
-        }
-        else {
-            // try to claim controller
-            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                // move towards the controller
-                creep.moveTo(creep.room.controller);
-            }
-        }
+  run: function(creep) {
+    if (creep.room.name != creep.memory.target) {
+      var exit = creep.room.findExitTo(creep.memory.target);
+      creep.moveTo(creep.pos.findClosestByRange(exit));
+    } else {
+      if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(creep.room.controller);
+      }
     }
+  }
 };
