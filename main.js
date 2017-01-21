0af608256091a058d780349ce680174a80b6d217
Unknown <emtffkev@gmail.com>
2017-01-21 02:15:11 -0500

    Test with refactor
    th_pion code...still don't work

--- main.js
+++ main.js
@@ -1,86 +1,35 @@
-require('prototype.spawn')();
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleRepairer = require('role.repairer');
-var roleWallRepairer = require('role.wallRepairer');
+// import modules
+require('prototype.creep');
+require('prototype.tower');
+require('prototype.spawn');
 
 module.exports.loop = function() {
-
-    for (var rooms in Game.rooms) {
-      //need larger scope.
-        room = Game.rooms[rooms];
-        spawn = room.find(FIND_MY_SPAWNS)[0];
-    }
+    // check for memory entries of died creeps by iterating over Memory.creeps
     for (let name in Memory.creeps) {
-        if (!Game.creeps[name]) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
             delete Memory.creeps[name];
         }
     }
 
+    // for each creeps
     for (let name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if (creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        } else if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        } else if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        } else if (creep.memory.role == 'repairer') {
-            roleRepairer.run(creep);
-        } else if (creep.memory.role == 'wallRepairer') {
-            roleWallRepairer.run(creep);
-        }
+        // run creep logic
+        Game.creeps[name].runRole();
     }
 
-    var towers = room.find(FIND_STRUCTURES, {
-        filter: (s) => s.structureType == STRUCTURE_TOWER
-    });
+    // find all towers
+    var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
+    // for each tower
     for (let tower of towers) {
-        var target = tower.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-        if (target != undefined) {
-            tower.attack(target);
-        }
+        // run tower logic
+        tower.defend();
     }
 
-    var minHarvesters = 3;
-    var minUpgraders = 1;
-    var minBuilders = 1;
-    var minRepairers = 1;
-    var minWallRepairers = 0;
-
-    var numHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
-    var numUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
-    var numBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
-    var numRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
-    var numWallRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'wallRepairer');
-
-    var energy = room.energyCapacityAvailable;
-    console.log('Harvesters: ' + numHarvesters + '. Upgraders: ' + numUpgraders + '. Builders: ' + numBuilders + '. Repairers: ' + numRepairers + '. Wall Repairers: ' + numWallRepairers + '.');
-
-    //removed duplicate energy line.
-    
-    var name = undefined;
-
-    if (numHarvesters < minHarvesters) {
-        name = spawn.createCustomCreep(energy, 'harvester');
-
-        if (name == ERR_NOT_ENOUGH_ENERGY && numHarvesters == 0) {
-            name = spawn.createCustomCreep(spawn.room.energyAvailable, 'harvester');
-        }
-    } else if (numUpgraders < minUpgraders) {
-        name = spawn.createCustomCreep(energy, 'upgrader');
-    } else if (numRepairers < minRepairers) {
-        name = spawn.createCustomCreep(energy, 'repairer');
-    } else if (numBuilders < minBuilders) {
-        name = spawn.createCustomCreep(energy, 'builder');
-    } else if (numWallRepairers < minWallRepairers) {
-        name = spawn.createCustomCreep(energy, 'wallRepairer');
-    } else {
-        name = spawn.createCustomCreep(energy, 'builder');
-    }
-
-    if (!(name < 0)) {
-        console.log('Spawned new creep: ' + name);
+    // for each spawn
+    for (let spawnName in Game.spawns) {
+        // run spawn logic
+        Game.spawns[spawnName].spawnCreepsIfNecessary();
     }
 };
